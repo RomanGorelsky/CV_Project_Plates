14802bd184ad19dacf9db48829ea4c382ef99f42
IS <88130237+BatarchiZ@users.noreply.github.com>
2025-03-28 07:12:27 +0300

    minor fixes

--- inference.py
+++ inference.py
@@ -1,25 +1,24 @@
 from tensorflow import keras
 from keras import losses, metrics, ops
-
 import logging
 import pathlib
 import random
-
 import cv2
 import keras
 import numpy as np
 import numpy.typing as npt
-
 import pathlib
 import logging
 import pathlib
 import random
+from os import PathLike
+import yaml
+from pydantic import BaseModel, computed_field, model_validator
+
 
 """
 Custom metrics and loss functions.
 """
-
-
 def cat_acc_metric(max_plate_slots: int, vocabulary_size: int):
     """
     Categorical accuracy metric.
@@ -96,26 +95,14 @@ def cce_loss(vocabulary_size: int, label_smoothing: float = 0.2):
     return cce
 
 
-"""
-Config values used throughout the code.
-"""
-
-from os import PathLike
-
-import yaml
-from pydantic import BaseModel, computed_field, model_validator
-
-
 class PlateOCRConfig(BaseModel, extra="forbid", frozen=True):
     """
     Model License Plate OCR config.
     """
-
     max_plate_slots: int
     """
     Max number of plate slots supported. This represents the number of model classification heads.
     """
-
     alphabet: str
     """
     All the possible character set for the model output.
@@ -129,9 +116,7 @@ class PlateOCRConfig(BaseModel, extra="forbid", frozen=True):
     Image height which is fed to the model.
     """
     img_width: int
-    """
-    Image width which is fed to the model.
-    """
+
 
     @computed_field  # type: ignore[misc]
     @property
@@ -152,45 +137,8 @@ def load_config_from_yaml(yaml_file_path: str | PathLike[str]) -> PlateOCRConfig
     config = PlateOCRConfig(**yaml_content)
     return config
 
-
-def load_config_from_yaml(yaml_file_path: str | PathLike[str]) -> PlateOCRConfig:
-    """Read and parse a yaml containing the Plate OCR config."""
-    with open(yaml_file_path, encoding="utf-8") as f_in:
-        yaml_content = yaml.safe_load(f_in)
-    config = PlateOCRConfig(**yaml_content)
-    return config
-
-
 def one_hot_plate(plate: str, alphabet: str) -> list[list[int]]:
     return [[0 if char != letter else 1 for char in alphabet] for letter in plate]
-
-
-def target_transform(
-    plate_text: str,
-    max_plate_slots: int,
-    alphabet: str,
-    pad_char: str,
-) -> npt.NDArray[np.uint8]:
-    # Pad the plates which length is smaller than 'max_plate_slots'
-    plate_text = plate_text.ljust(max_plate_slots, pad_char)
-    # Generate numpy arrays with one-hot encoding of plates
-    encoded_plate = np.array(one_hot_plate(plate_text, alphabet=alphabet), dtype=np.uint8)
-    return encoded_plate
-
-
-def read_plate_image(image_path: str, img_height: int, img_width: int) -> npt.NDArray:
-    """
-    Read and resize a license plate image.
-
-    :param image_path: The path to the license plate image.
-    :param img_height: The desired height of the resized image.
-    :param img_width: The desired width of the resized image.
-    :return: The resized license plate image as a NumPy array.
-    """
-    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    img = cv2.resize(img, (img_width, img_height), interpolation=cv2.INTER_LINEAR)
-    img = np.expand_dims(img, -1)
-    return img
 
 
 def load_keras_model(
@@ -215,27 +163,6 @@ IMG_EXTENSIONS: set[str] = {".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tiff", ".
 """Valid image extensions for the scope of this script."""
 
 
-def load_images_from_folder(
-    img_dir: pathlib.Path,
-    width: int,
-    height: int,
-    shuffle: bool = False,
-    limit: int | None = None,
-) -> list[npt.NDArray]:
-    """
-    Return all images read from a directory. This uses the same read function used during training.
-    """
-    image_paths = sorted(
-        str(f.resolve()) for f in img_dir.iterdir() if f.is_file() and f.suffix in IMG_EXTENSIONS
-    )
-    if limit:
-        image_paths = image_paths[:limit]
-    if shuffle:
-        random.shuffle(image_paths)
-    images = [read_plate_image(i, img_height=height, img_width=width) for i in image_paths]
-    return images
-
-
 def postprocess_model_output(
     prediction: npt.NDArray,
     alphabet: str,
@@ -251,67 +178,6 @@ def postprocess_model_output(
     plate = "".join([alphabet[x] for x in prediction])
     return plate, probs
 
-
-def low_confidence_positions(probs, thresh=0.3) -> npt.NDArray:
-    """Returns indices of elements in `probs` less than `thresh`, indicating low confidence."""
-    return np.where(np.array(probs) < thresh)[0]
-
-
-def display_predictions(
-    image: npt.NDArray,
-    plate: str,
-    probs: npt.NDArray,
-    low_conf_thresh: float,
-) -> None:
-    """
-    Display plate and corresponding prediction.
-    """
-    plate_str = "".join(plate)
-    logging.info("Plate: %s", plate_str)
-    logging.info("Confidence: %s", probs)
-    image_to_show = cv2.resize(image, None, fx=3, fy=3, interpolation=cv2.INTER_LINEAR)
-    # Converting to BGR for color text
-    image_to_show = cv2.cvtColor(image_to_show, cv2.COLOR_GRAY2RGB)
-    # Average probabilities
-    avg_prob = np.mean(probs) * 100
-    cv2.putText(
-        image_to_show,
-        f"{plate_str}  {avg_prob:.{2}f}%",
-        org=(5, 30),
-        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-        fontScale=1,
-        color=(0, 0, 0),
-        lineType=1,
-        thickness=6,
-    )
-    cv2.putText(
-        image_to_show,
-        f"{plate_str}  {avg_prob:.{2}f}%",
-        org=(5, 30),
-        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-        fontScale=1,
-        color=(255, 255, 255),
-        lineType=1,
-        thickness=2,
-    )
-    # Display character with low confidence
-    low_conf_chars = "Low conf. on: " + " ".join(
-        [plate[i] for i in low_confidence_positions(probs, thresh=low_conf_thresh)]
-    )
-    cv2.putText(
-        image_to_show,
-        low_conf_chars,
-        org=(5, 200),
-        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-        fontScale=0.7,
-        color=(0, 0, 220),
-        lineType=1,
-        thickness=2,
-    )
-    cv2.imshow("plates", image_to_show)
-    if cv2.waitKey(0) & 0xFF == ord("q"):
-        return
-
 if __name__ == "__main__":
     model_path = r"C:\\Users\\Acer\\Desktop\\cnn_ocr-epoch_728-acc_0.922.keras"
     config_file = r"C:\\Users\\Acer\\Desktop\\config.yaml"
@@ -324,19 +190,5 @@ if __name__ == "__main__":
     model = load_keras_model(
         model_path, vocab_size=config.vocabulary_size, max_plate_slots=config.max_plate_slots
     )
-    images = load_images_from_folder(
-        img_dir, width=config.img_width, height=config.img_height
-    )
-    for image in images:
-        x = np.expand_dims(image, 0)
-        prediction = model(x, training=False)
-        prediction = keras.ops.stop_gradient(prediction).numpy()
-        plate, probs = postprocess_model_output(
-            prediction=prediction,
-            alphabet=config.alphabet,
-            max_plate_slots=config.max_plate_slots,
-            vocab_size=config.vocabulary_size,
-        )
-        print(plate)
 
     cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -6,30 +6,32 @@ import numpy as np
 from inference import load_keras_model, load_config_from_yaml, postprocess_model_output
 import keras
 
-# from fast_plate_ocr import ONNXPlateRecognizer
-
-# m = ONNXPlateRecognizer('argentinian-plates-cnn-model')
-
+# Path to CNN model
 model_path = r"cnn_ocr-epoch_728-acc_0.922.keras"
+# Path to config file
 config_file = r"config.yaml"
+
+# Load config file
 config = load_config_from_yaml(config_file)
+
+# Load CNN model
 model = load_keras_model(
     model_path, vocab_size=config.vocabulary_size, max_plate_slots=config.max_plate_slots
 )
 
-# reader = easyocr.Reader(['en'])
+# Conenction to SQLite server
 conn = sqlite3.connect('car_db.sqlite')
 cursor = conn.cursor()
 
+# Parameters for VideoCapture
 cap = cv2.VideoCapture(0)
-
 last_check = time.time()
 check_interval = 5  # seconds
 label = ""
 label_display_time = 3  # show label for 3 seconds
 label_shown_at = None
 
-
+# Create empty table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS database (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -38,6 +40,7 @@ CREATE TABLE IF NOT EXISTS database (
 """)
 conn.commit()
 
+# Fill table with info from database.txt
 with open('database.txt', 'r') as f:
     for line in f:
         plate = line.strip()
@@ -50,20 +53,20 @@ conn.commit()
 
 
 while True:
+    # Read from web_camera
     ret, frame = cap.read()
     if not ret:
         break
 
     current_time = time.time()
 
-    # Perform OCR check every 10 seconds
+    # Perform OCR check every 5 seconds
     if current_time - last_check >= check_interval:
         last_check = current_time
         label_shown_at = current_time
         label = "No plate detected"  # default label
 
         # OCR text detection
-
         frame_2 = cv2.resize(frame, (140,70))
         frame_2 = cv2.cvtColor(frame_2, cv2.COLOR_BGR2GRAY)
         frame_2 = np.expand_dims(frame_2, 0)
@@ -79,20 +82,12 @@ while True:
         text = plate
         text = text.replace('_', "")
 
-        # text = m.run("image.png")[0]
-        # print(text)
-
-
-        # for text in results:
+        # Check that correct text was found
         if len(text) >= 5:
-            # text = text.replace(" ", "")
-            # text = text.upper()
-            # print(text)
             # Check in DB
             cursor.execute("SELECT EXISTS(SELECT 1 FROM database WHERE plate=?)", (text,))
             found = cursor.fetchone()[0]
             label = f"Plate: {text} | {'Found' if found else 'Not found'}"
-            # break  # stop after first match
 
     if label and label_shown_at and (current_time - label_shown_at > label_display_time):
         label = ""
